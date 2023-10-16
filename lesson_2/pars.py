0ef192ab643067e42925b48e55904f19a134f49c
zaxa <arinushkin.k.a@gmail.com>
2023-10-16 11:49:17 +0300

    Parsing json_text

---
+++ lesson_2/pars.py
@@ -0,0 +1,19 @@
+import json
+
+json_text = ('{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},'
+             '{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}')
+
+json_pars = json.loads(json_text)
+
+print(json_pars['messages'][1]['message'])
+
+
+
+
+
+
+
+
+
+
+
